470fce7c2cf9629454b0e141d63f27ae1d0a4ba9
Jasiel Dsilva <dsilvajasiel@gmail..com>
2026-04-28 22:58:09 +0530

    progressBar

---
+++ progressBar/components/ProgressBar.jsx
@@ -0,0 +1,29 @@
+import React, { useActionState, useEffect, useState } from 'react'
+import { MAX,MIN } from '../constants'
+
+const ProgressBar = ({value =0,onComplete=()=>{}}) => {
+    const [percent,setPercent] = useState(value)
+    useEffect(()=>{
+        setPercent(Math.min(MAX,Math.max(value,MIN)))
+        if(value >= MAX){
+            onComplete()
+        }
+    },[value])
+  return (
+    <div className='progress'>
+      <span
+      style={{color:percent>49?"white":"black"}}
+      >{percent.toFixed()}%</span>
+        <div style={{transform:`scaleX(${percent/MAX})`,
+        transformOrigin:"left"}}
+        //style={{width: `${percent}%`}} 
+        ///width is bad for performance can be a follow up
+        role='progressbar'
+        aria-valuemin={MIN}
+        aria-valuemax={MAX}
+        aria-valuenow={percent.toFixed()}/>
+    </div>
+  )
+}
+
+export default ProgressBar
